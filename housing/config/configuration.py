aa47e698f25e7a65a0f08731876f3ea1df7581bf
Manjunath-Bhashyam <manjunathbhashyam1995@gmail.com>
2022-06-30 21:34:23 +0530

    Further updates in util.py and configuration.py

--- housing/config/configuration.py
+++ housing/config/configuration.py
@@ -1,9 +1,22 @@
-from housing.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelEvaluationConfig, ModelPusherConfig, ModelTrainerConfig, TrainingPipelineConfig
+from housing.entity.config_entity import DataIngestionConfig,DataValidationConfig, \
+    DataTransformationConfig,ModelEvaluationConfig,ModelPusherConfig,ModelTrainerConfig,TrainingPipelineConfig
+from housing.util.util import read_yaml_file
+import os, sys
+from housing.constant import *
+from housing.exception import HousingException
+from housing.logger import logging
 
 class Configuration:
-
-    def __init__(self) -> None:
-        pass
+    
+    def __init__(self,
+        config_file_path: str = CONFIG_FILE_PATH,
+        current_time_stamp: str = CURRENT_TIME_STAMP) -> None:
+        try:
+            self.config_info  = read_yaml_file(file_path=config_file_path)
+            self.training_pipeline_config = self.get_training_pipeline_config()
+            self.time_stamp = current_time_stamp
+        except Exception as e:
+            raise HousingException(e,sys) from e
 
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         pass
@@ -24,4 +37,14 @@ class Configuration:
         pass
 
     def get_training_pipeline_config(self) -> TrainingPipelineConfig:
-        pass
+        try:
+            training_pipeline_config = self.config_info[TRAINING_PIPELINE_CONFIG_KEY]
+            artifact_dir = os.path.join(ROOT_DIR,
+            training_pipeline_config[TRAINING_PIPELINE_NAME_KEY],
+            training_pipeline_config[TRAINING_PIPELINE_ARTIFACT_DIR_KEY])
+
+            training_pipeline_config = TrainingPipelineConfig(artifact_dir=artifact_dir)
+            logging.info(f"Training pipeline config: {training_pipeline_config}")
+            return training_pipeline_config
+        except Exception as e:
+            raise HousingException(e,sys) from e
