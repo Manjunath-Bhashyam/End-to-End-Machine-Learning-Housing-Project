0fc31438f075dbc9af9437230e7673d273610476
Manjunath-Bhashyam <manjunathbhashyam1995@gmail.com>
2022-07-03 08:57:22 +0530

    further updates

--- housing/exception/__init__.py
+++ housing/exception/__init__.py
@@ -6,7 +6,8 @@ class HousingException(Exception):
     def __init__(self, error_message:Exception,error_detail:sys):
         super().__init__(error_message)
         self.error_message=HousingException.get_detailed_error_message(error_message=error_message,
-                                                                       error_detail=error_detail)
+                                                                       error_detail=error_detail
+                                                                        )
 
 
     @staticmethod
@@ -16,9 +17,15 @@ class HousingException(Exception):
         error_detail: object of sys module
         """
         _,_ ,exec_tb = error_detail.exc_info()
-        line_number = exec_tb.tb_frame.f_lineno
+        exception_block_line_number = exec_tb.tb_frame.f_lineno
+        try_block_line_number = exec_tb.tb_lineno
         file_name = exec_tb.tb_frame.f_code.co_filename
-        error_message = f"Error occured in scrip: [{file_name}] at line number: [{line_number}] error message: [{error_message}]"
+        error_message = f"""
+        Error occured in script: 
+        [ {file_name} ] at 
+        try block line number: [{try_block_line_number}] and exception block line number: [{exception_block_line_number}] 
+        error message: [{error_message}]
+        """
         return error_message
 
     def __str__(self):
--- housing/pipeline/pipeline.py
+++ housing/pipeline/pipeline.py
@@ -1,28 +1,28 @@
-from housing.component.data_ingestion import DataIngestion
 from housing.config.configuration import Configuration
 from housing.logger import logging
 from housing.exception import HousingException
 
 from housing.entity.artifact_entity import DataIngestionArtifact
 from housing.entity.config_entity import DataIngestionConfig
-
-import sys, os
+from housing.component.data_ingestion import DataIngestion
+import os,sys
 
 class Pipeline:
 
-    def __init__(self, config: Configuration = Configuration()) -> None:
+    def __init__(self,config: Configuration = Configuration()) -> None:
         try:
-            self.config = config
+            self.config=config
 
         except Exception as e:
             raise HousingException(e,sys) from e
 
     def start_data_ingestion(self)->DataIngestionArtifact:
         try:
-            data_ingestion = DataIngestion(data_ingestion_config = self.config.get_data_ingestion_config())
+            data_ingestion = DataIngestion(data_ingestion_config=self.config.get_data_ingestion_config())
             return data_ingestion.initiate_data_ingestion()
         except Exception as e:
-            raise HousingException(e,sys) from e
+            raise HousingException(e,sys) from e    
+
 
     def start_data_validation(self):
         pass
@@ -41,9 +41,10 @@ class Pipeline:
 
     def run_pipeline(self):
         try:
-            # Data Ingestion
+            #data ingestion
 
-            data_ingestion_artifact = self.start_data_ingestion()
+            data_ingestion_artifact = self.start_data_ingestion()           
+
 
         except Exception as e:
             raise HousingException(e,sys) from e
